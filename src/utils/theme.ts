7afd237875d31182a3b301213460840378377080
xomod <xomod99@gmail.com>
2020-12-19 15:35:41 +0100

    :lipstick: Add new theme color

--- src/utils/theme.ts
+++ src/utils/theme.ts
@@ -1,19 +1,28 @@
 import { createMuiTheme } from "@material-ui/core";
 
-export const LIGHT_THEME = createMuiTheme({
+export const PRIMARY_THEME = createMuiTheme({
     palette: {
-        type: "light",
+        type: "dark",
         primary: {
-            main: "#99CC66"
+            main: "#344966"
         }
     }
 });
 
-export const DARK_THEME = createMuiTheme({
+export const SECONDARY_THEME = createMuiTheme({
+    palette: {
+        type: "light",
+        primary: {
+            main: "#0496FF"
+        }
+    }
+});
+
+export const TEXT_THEME = createMuiTheme({
     palette: {
         type: "dark",
         primary: {
-            main: "#99CC66"
+            main: "#202b3b"
         }
     }
 });
